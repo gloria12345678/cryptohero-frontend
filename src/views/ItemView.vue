ac0bc7413aa1bf359723bc7985f601325abcfbfa
gloria12345678 <quyi28266687@gmail.com>
2018-06-07 19:31:31 +0800

    Revert "f'f"
    This reverts commit e7f8d81ed5d334dd48984e0d76dd2e2c987b5b9a.

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -145,7 +145,7 @@ export default {
       return `http://test.cdn.hackx.org/heros/${this.itemId}.jpg`;
     },
     getCardBackSideImage() {
-      return `http://test.cdn.hackx.org/back/${this.itemId}.jpeg`;
+      return `http://test.cdn.hackx.org/back/back_${this.itemId}.jpg`;
     },
     isOwner() {
       return this.item.owner === this.me.address;
